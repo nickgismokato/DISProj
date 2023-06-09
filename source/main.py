a5388e95cf1ef563a6782b5c7f0c901c70aad671
nickgismokato <nickvillumlaursen@gmail.com>
2023-06-09 07:40:39 +0200

    Added simple login page

--- source/main.py
+++ source/main.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from flask.views import View
 import random as rand
 
 app = Flask(__name__)
+
+
 
 #Localhost:8080/
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/auth/login", methods=['GET','POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('auth/login.html', error=error)
 
 @app.route("/user/<name>")
 def user(name):
@@ -18,4 +31,5 @@ def root():
     return render_template('map.html')
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(host="localhost", port=8080, debug=True)
