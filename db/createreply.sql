daa0fdb0c923afaa8c164d1292b0bee62324b255
Asger Fihl Hedegaard Nielsen <asger@fihl.net>
2023-06-10 13:18:12 +0200

    Comments, delete post, create reply

---
+++ db/createreply.sql
@@ -0,0 +1,2 @@
+INSERT INTO replies(content, replydate, uid_pk, pid_pk)
+Values (%s, %s, %s, %s)
--- db/db_interface.py
+++ db/db_interface.py
@@ -2,7 +2,7 @@ import psycopg2
 from datetime import date
 from config import config
 
-
+#Initialize database
 def db_init():
     conn = None
     conf = config()
@@ -30,9 +30,11 @@ def db_init():
             cur.close()
             conn.close()
 
+#login a user, arguments: username = string, name of user. password = string, user password
 def login(username, password):
     return
 
+#Register a user, arguments: username = string, name of user. password = string, user password
 def register(username, password):
     today = date.today()
     conn = None
@@ -63,6 +65,7 @@ def register(username, password):
             cur.close()
             conn.close()
 
+#Delete a user, arguments: username = string, name of user
 def deleteuser(username):
     conn = None
     conf = config()
@@ -92,6 +95,7 @@ def deleteuser(username):
             cur.close()
             conn.close()
 
+#Create a post, arguments: username = string, name of user. municipalityname = string, name of municipality. text = string, content of post
 def createpost(username, municipalityname, text):
     today = date.today()
     conn = None
@@ -137,12 +141,80 @@ def createpost(username, municipalityname, text):
             cur.close()
             conn.close()
 
-def createreply(username, post, text):
-    return
+#Delete a post, arguments: postid = integer, id of post
+def deletepost(postid):
+    conn = None
+    conf = config()
+
+    try:
+        #connection
+        conn = psycopg2.connect(conf)
+
+        #cursor
+        cur = conn.cursor()
+
+        #fetch init script
+        with open('deletepost.sql','r') as delete_file:
+            content = delete_file.read()
+
+        values = (postid)
+
+        cur.execute(content, [values])
+
+        conn.commit()
+
+    except Exception as error:
+        print(error)
+
+    finally:
+        if conn != None:
+            cur.close()
+            conn.close()
+
+#Create a reply, arguments: username = string, name of user. postid = integer, id of post. text = string, content of reply
+def createreply(username, postid, text):
+    today = date.today()
+    conn = None
+    conf = config()
+
+    try:
+        #connection
+        conn = psycopg2.connect(conf)
+
+        #cursor
+        cur = conn.cursor()
+
+        #fetch init script
+        with open('createreply.sql','r') as post_file:
+            content = post_file.read()
+
+        cur.execute('''
+        SELECT uid
+        FROM users
+        WHERE username = %s
+        ''', [username])
+        userpk = cur.fetchone()
+
+
+
+        values = (text, today, userpk[0], postid)
+
+        cur.execute(content, values)
+
+        conn.commit()
+
+    except Exception as error:
+        print(error)
+
+    finally:
+        if conn != None:
+            cur.close()
+            conn.close()
 
 def getmunicipality(name):
     return
 
+#Subscribes a user to a municipality, arguments: uesrname = string, name of user. municipalityname = string, name of municipality
 def subscribe(username, municipalityname):
     today = date.today()
     conn = None
@@ -187,3 +259,4 @@ def subscribe(username, municipalityname):
         if conn != None:
             cur.close()
             conn.close()
+
